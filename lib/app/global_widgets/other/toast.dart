9d1a473cfdfe59c9c239be3f50607905d9ad2a01
hanifabdullah <hanifabdullah7@gmail.com>
2023-02-16 13:45:02 +0700

    Fix bug and add privacy policy

--- lib/app/global_widgets/other/toast.dart
+++ lib/app/global_widgets/other/toast.dart
@@ -1,10 +1,11 @@
 import 'package:absensi_kegiatan/app/utils/colors.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 
 showToast(String message) {
-  Fluttertoast.showToast(
-      msg: message,
-      backgroundColor: basicRed1,
-      webPosition: "right",
-      webBgColor: "linear-gradient(to right, #e93838, #e93838)");
+  Get.snackbar("Perhatian", message);
+  // Fluttertoast.showToast(
+  //     msg: message,
+  //     backgroundColor: basicRed1,
+  //     webPosition: "right",
+  //     webBgColor: "linear-gradient(to right, #e93838, #e93838)");
 }
